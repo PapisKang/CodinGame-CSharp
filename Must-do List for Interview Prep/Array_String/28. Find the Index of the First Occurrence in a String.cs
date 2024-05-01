4d72f01fcb299446ce0781994611017799b34584
Ndiaye Pape ngagne <papengagne1997@gmail.com>
2024-05-01 23:30:08 +0200

    28. Find the Index of the First Occurrence in a String

---
+++ Must-do List for Interview Prep/Array_String/28. Find the Index of the First Occurrence in a String.cs
@@ -0,0 +1,7 @@
+// https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/?envType=study-plan-v2&envId=top-interview-150
+
+public class Solution {
+    public int StrStr(string haystack, string needle) {
+        return haystack.IndexOf(needle);
+    }
+}
